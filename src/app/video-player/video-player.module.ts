f14eb8fb0d4fdc2d2e75d4e980e44d0786d20bee
lisacopeland <lisacope@msn.com>
2020-01-02 11:07:17 -0800

    styling

--- src/app/video-player/video-player.module.ts
+++ src/app/video-player/video-player.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { VideoPlayerComponent } from './video-player.component';
 
 @NgModule({
   imports: [
     YouTubePlayerModule,
+    FlexLayoutModule
   ],
   declarations: [VideoPlayerComponent],
   exports: [VideoPlayerComponent]
